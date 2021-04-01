0a752aee1878913dff506b241f904fb4600661a0
shah-flip <shah.xhtml@yahoo.com>
2021-04-01 06:04:19 +0530

    disable auto generation of Stylesheets and helper files

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RailsVuejsTutorials
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.stylesheets = false
+    config.generators.helper = false
   end
 end
