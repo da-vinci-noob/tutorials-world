dd6c99331f70851dac79969e355dfd3c98a62085
shah-flip <shah.xhtml@yahoo.com>
2021-04-02 04:54:15 +0530

    Uncomment Vue Turbolink config

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -52,21 +52,21 @@
 // yarn add vue-turbolinks
 //
 // Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
+
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#hello',
+    data: () => {
+      return {
+        message: "Can you say hello?"
+      }
+    },
+    components: { App }
+  })
+})
