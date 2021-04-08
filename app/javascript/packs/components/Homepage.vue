320d815c5ceffd43484e287a2a1687945582fd76
shah-flip <shah.xhtml@yahoo.com>
2021-04-09 02:11:23 +0530

    Layout and Alerts Update

--- app/javascript/packs/components/Homepage.vue
+++ app/javascript/packs/components/Homepage.vue
@@ -1,26 +1,24 @@
 <template>
-  <div class="container-block">
-    <div class="space-y-4 inside-container">
-      <div class="flex flex-row items-center justify-between">
-        <div class="w-1/4"></div>
-        <div class="text-2xl font-bold text-center lg:text-3xl xl:text-5xl">
-          Tutorials
-        </div>
-        <div class="w-1/4 text-right">
-          <slot name="add_new_link"></slot>
-        </div>
+  <div class="space-y-4">
+    <div class="flex flex-row items-center justify-between">
+      <div class="w-1/4"></div>
+      <div class="text-2xl font-bold text-center lg:text-3xl xl:text-5xl">
+        Tutorials
       </div>
-      <div>
-        <h2 class="text-xl font-bold">Languages</h2>
-        <div class="mt-2">
-          <slot name="language_buttons"></slot>
-        </div>
+      <div class="w-1/4 text-right">
+        <slot name="add_new_link"></slot>
       </div>
-      <div>
-        <h2 class="text-xl font-bold">Others</h2>
-        <div class="mt-2">
-          <slot name="others_buttons"></slot>
-        </div>
+    </div>
+    <div>
+      <h2 class="text-xl font-bold">Languages</h2>
+      <div class="mt-2">
+        <slot name="language_buttons"></slot>
+      </div>
+    </div>
+    <div>
+      <h2 class="text-xl font-bold">Others</h2>
+      <div class="mt-2">
+        <slot name="others_buttons"></slot>
       </div>
     </div>
   </div>
--- app/javascript/packs/components/Tutorials/ShowTutorial.vue
+++ app/javascript/packs/components/Tutorials/ShowTutorial.vue
@@ -1,61 +1,57 @@
 <template>
-  <div class="container-block">
-    <div class="relative py-0 mt-10 inside-container">
-      <button
-        class="absolute top-0 left-0 px-4 py-2 bg-green-400 hover:bg-green-300 rounded-tl-xl rounded-br-xl"
-        onclick="window.history.back()"
+  <div>
+    <button
+      class="absolute top-0 left-0 px-4 py-2 bg-green-400 hover:bg-green-300 rounded-tl-xl rounded-br-xl"
+      onclick="window.history.back()"
+    >
+      BACK
+    </button>
+    <div>
+      <span
+        class="block text-2xl font-bold text-center lg:text-3xl xl:text-5xl"
       >
-        BACK
-      </button>
-      <div>
-        <span
-          class="block text-2xl font-bold text-center lg:text-3xl xl:text-5xl"
-        >
-          {{ language.title }}
+        {{ language.title }}
+      </span>
+    </div>
+    <div class="mt-2 text-center lg:mt-4">
+      <input
+        class="px-4 py-1 rounded-lg focus:outline-none focus:ring-indigo-400 focus:ring-2 active:ring-indigo-400 active:ring-2"
+        type="text"
+        autofocus
+        v-model="searchQuery"
+        placeholder="Search"
+      />
+    </div>
+    <div class="mt-8 space-y-2">
+      <div
+        v-for="tutorial in resultQuery"
+        :key="tutorial.id"
+        class="border-b border-gray-400 border-dashed"
+      >
+        <span class="text-lg font-semibold sm:text-xl lg:text-2xl xl:text-3xl">
+          Query:
         </span>
-      </div>
-      <div class="mt-2 text-center lg:mt-4">
-        <input
-          class="px-4 py-1 rounded-lg focus:outline-none focus:ring-indigo-400 focus:ring-2 active:ring-indigo-400 active:ring-2"
-          type="text"
-          autofocus
-          v-model="searchQuery"
-          placeholder="Search"
-        />
-      </div>
-      <div class="mt-8 space-y-2">
-        <div
-          v-for="tutorial in resultQuery"
-          :key="tutorial.id"
-          class="border-b border-gray-400 border-dashed"
-        >
-          <span
-            class="text-lg font-semibold sm:text-xl lg:text-2xl xl:text-3xl"
+        <span class="text-lg sm:text-xl lg:text-2xl xl:text-3xl">
+          {{ tutorial.title }}
+        </span>
+        <p>
+          <button
+            :id="tutorial.id"
+            @click.prevent="toggleStepsOpen(tutorial.id)"
+            class="text-sm text-indigo-700 focus:outline-none"
           >
-            Query:
-          </span>
-          <span class="text-lg sm:text-xl lg:text-2xl xl:text-3xl">
-            {{ tutorial.title }}
-          </span>
-          <p>
-            <button
-              :id="tutorial.id"
-              @click.prevent="toggleStepsOpen(tutorial.id)"
-              class="text-sm text-indigo-700 focus:outline-none"
-            >
-              Toggle Solution
-            </button>
-          </p>
-          <collapse-transition>
-            <div v-show="isOpen[tutorial.id]">
-              <div class="p-2">
-                <div class="px-4 py-1 bg-gray-200 rounded-2xl">
-                  <slot :name="`tutorial_body_${tutorial.id}`"></slot>
-                </div>
+            Toggle Solution
+          </button>
+        </p>
+        <collapse-transition>
+          <div v-show="isOpen[tutorial.id]">
+            <div class="p-2">
+              <div class="px-4 py-1 bg-gray-200 rounded-2xl">
+                <slot :name="`tutorial_body_${tutorial.id}`"></slot>
               </div>
             </div>
-          </collapse-transition>
-        </div>
+          </div>
+        </collapse-transition>
       </div>
     </div>
   </div>
